70c67c4ff5f3f078df9fd7b68d517128deabc20a
Justin Appave <justin.appave@ucalgary.ca>
2017-01-23 11:21:20 -0700

    Update Tiles.java

--- Tiles.java
+++ Tiles.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Tiles{
   public static void(String[] args){
-    
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Enter your player name: ");
+    String playerName = keyboard.nextLine();
   }
 }
