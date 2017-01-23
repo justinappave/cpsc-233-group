80efa9f0b58c6bd5eb898a69c7b8769766730aac
Justin Appave <justin.appave@ucalgary.ca>
2017-01-23 11:26:04 -0700

    Update Tiles.java

--- Tiles.java
+++ Tiles.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class Tiles{
-  public static void(String[] args){
+  public static void main(String[] args){
     Scanner keyboard = new Scanner(System.in);
     System.out.print("Enter your player name: ");
     String playerName = keyboard.nextLine();
+    int playerLength = playerName.length();
+    if(playerLength >= 10){
+      System.out.println("Error. Too long!");
+    }
   }
 }
