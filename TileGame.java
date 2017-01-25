80478949a2ce7adaed87c031b148202f8a6fbf66
Justin Appave <justin.appave@ucalgary.ca>
2017-01-25 11:51:10 -0700

    Add files via upload

---
+++ TileGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TileGame{
+	static char[][] startConfig = {
+		{'1',' ',' '},
+		{' ',' ',' '},
+		{' ',' ',' '}
+	};
+	
+	public static void main(String[] args){
+	
+	char[][] grid = startConfig;
+	for (int row = 0; row < 3; row++) {
+			System.out.print("|");
+			for (int column = 0; column < 3; column++) {
+				System.out.print(grid[row][column] + "|");
+			}
+			System.out.println();
+		}
+	Scanner keyboard = new Scanner(System.in);
+	System.out.print("Enter row: ");
+	int enteredRow = keyboard.nextInt();
+	System.out.print("Enter column: ");
+	int enteredColumn = keyboard.nextInt();
+	}
+}
