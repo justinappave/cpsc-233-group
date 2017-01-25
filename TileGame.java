fa2d6db6cc92b68024d12854489e31d7589218d9
Justin Appave <justin.appave@ucalgary.ca>
2017-01-25 12:31:16 -0700

    Add files via upload

--- TileGame.java
+++ TileGame.java
@@ -6,21 +6,25 @@ public class TileGame{
 		{' ',' ',' '},
 		{' ',' ',' '}
 	};
+	static char[][] possibleXWin = {
+		{' ','1',' '},
+		{' ',' ',' '},
+		{' ',' ',' '}
+	};
 	
 	public static void main(String[] args){
 	
 	char[][] grid = startConfig;
-	for (int row = 0; row < 3; row++) {
-			System.out.print("|");
-			for (int column = 0; column < 3; column++) {
-				System.out.print(grid[row][column] + "|");
-			}
-			System.out.println();
-		}
-	Scanner keyboard = new Scanner(System.in);
-	System.out.print("Enter row: ");
-	int enteredRow = keyboard.nextInt();
-	System.out.print("Enter column: ");
-	int enteredColumn = keyboard.nextInt();
+	while(startConfig != possibleXWin){
+		for (int row = 0; row < 3; row++) {
+				System.out.print("|");
+				for (int column = 0; column < 3; column++) {
+					System.out.print(grid[row][column] + "|");}
+				System.out.println();}
+		{Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter row: ");
+		int enteredRow = keyboard.nextInt();
+		System.out.print("Enter column: ");
+		int enteredColumn = keyboard.nextInt();}}
 	}
 }
