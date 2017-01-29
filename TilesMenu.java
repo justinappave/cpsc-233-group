548385b71d77d08bf0b60cbcb1f4a559b071f4d4
Justin Appave <justin.appave@ucalgary.ca>
2017-01-29 15:24:28 -0700

    Add files via upload

--- TilesMenu.java
+++ TilesMenu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Tiles{
+public class TilesMenu{
   public static void main(String[] args){
     Scanner keyboard = new Scanner(System.in);
     int playerLength = 100;
