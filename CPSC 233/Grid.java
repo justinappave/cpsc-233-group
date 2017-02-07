ace9b5f64f3b2e57bebdbcfcc637158f842793e6
Justin Appave <justin.appave@ucalgary.ca>
2017-02-07 12:50:12 -0700

    Add files via upload

---
+++ CPSC 233/Grid.java
@@ -0,0 +1,14 @@
+public class Grid{
+static char[][] easyConfig = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7',' ','8'}
+};
+
+static char[][] mediumConfig = {
+  {'3','8','4'},
+  {' ','1','7'},
+  {'6','5','2'}
+};
+
+}
