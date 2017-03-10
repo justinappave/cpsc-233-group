bc2059ae0bca931289df10935a120e89406c863b
Justin Appave <justin.appave@ucalgary.ca>
2017-03-10 09:00:58 -0700

    Add files via upload

---
+++ tileGame/Leaderboard.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Leaderboard{
+    Scanner read;
+    PrintWriter write;
+
+    private String playerid;
+    private int playerscore;
+
+    int x = 0;
+    ArrayList<String> n = new ArrayList<String>();
+    ArrayList<Integer> s = new ArrayList<Integer>();
+    ArrayList <Leaderboard>players = new ArrayList<Leaderboard>();
+
+    public Leaderboard(String name, int score){
+        playerid = name;
+        playerscore = score;
+    }
+    public String getName(){
+        return playerid;
+    }
+
+    public int getScore(){
+        return playerscore;
+    }
+
+    public String toString (){
+        return playerid+"\t "+playerscore;
+    }
+}
