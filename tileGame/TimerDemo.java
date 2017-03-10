2966a2960b7787ef2d7da9d8ca31b4761e92f914
travnguyen <travis.nguyen1@ucalgary.ca>
2017-03-09 23:07:43 -0700

    Add files via upload

---
+++ tileGame/TimerDemo.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.util.Timer; //schedule thread to execute at certain time
+import java.util.TimerTask; //runnable interface, extend to add own timer
+
+public class TimerDemo {
+  Toolkit toolkit;
+
+  Timer timer;
+
+  public TimerDemo(int seconds) {
+    toolkit = Toolkit.getDefaultToolkit();
+    timer = new Timer();
+    timer.schedule(new RemindTask(), seconds * 1000); //time in milliseconds
+  }
+
+  class RemindTask extends TimerTask {
+    public void run() {
+      System.out.println("Time's up!");
+      toolkit.beep();//add sound when timers up
+      System.exit(0);//exit when seconds reach 0
+    }
+  }
+
+  public static void main(String args[]) {
+    System.out.println("Starting easy mode...");
+    new TimerDemo(5); //set amount of seconds to timer
+    System.out.println("Start!");
+  }
+}  
