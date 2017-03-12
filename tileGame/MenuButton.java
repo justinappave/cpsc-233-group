6e177bda6b0f2c21784d1cd2f8c2daa1af2952e0
travnguyen <travis.nguyen1@ucalgary.ca>
2017-03-12 13:28:34 -0600

    Add files via upload

--- tileGame/MenuButton.java
+++ tileGame/MenuButton.java
@@ -9,22 +9,41 @@ import javax.swing.JPanel;
 public class MenuButton {
 public static void main(String[] args){
 	
-	JFrame frame = new JFrame("Test");
+	JFrame frame = new JFrame("Tile Game");
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(300,300);
+	frame.setSize(500,500);
 	JPanel panel = new JPanel();
 	frame.add(panel);
-	JButton button = new JButton("Click here");
-	panel.add(button);
-	button.addActionListener(new Action());
+	
+	JButton quickButton = new JButton("Quickplay");
+	JButton menuButton = new JButton("Menu");
+	JButton exitButton = new JButton("Exit");
+	
+	panel.add(quickButton);
+	panel.add(menuButton);
+	panel.add(exitButton);
+	
+	quickButton.addActionListener(new quickAction());
+	menuButton.addActionListener(new menuAction());
 }
-	static class Action implements ActionListener{
+	static class quickAction implements ActionListener{
 		public void actionPerformed (ActionEvent e){
-			JFrame frame2 = new JFrame("Clicked");
+			JFrame frame1 = new JFrame("Quickplay");
+			frame1.setVisible(true);
+			frame1.setSize(500,500);
+			JLabel label = new JLabel("Starting Quickplay...");
+			JPanel panel = new JPanel();
+			frame1.add(panel);
+			panel.add(label);
+		}
+	}
+	static class menuAction implements ActionListener{
+		public void actionPerformed (ActionEvent e){
+			JFrame frame2 = new JFrame("Main Menu");
 			frame2.setVisible(true);
-			frame2.setSize(200,200);
-			JLabel label = new JLabel("You clicked me!");
+			frame2.setSize(500,500);
+			JLabel label = new JLabel("Moving to Main Menu...");
 			JPanel panel = new JPanel();
 			frame2.add(panel);
 			panel.add(label);
