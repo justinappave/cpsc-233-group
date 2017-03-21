bd3d628c59a62f3147c71e48062ef511c22fe3b8
travnguyen <travis.nguyen1@ucalgary.ca>
2017-03-20 21:52:43 -0600

    PauseMenu (GUI) - in progress

---
+++ pauseMenu/testing/PauseTest.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class PauseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PauseGUI pause = new PauseGUI();
+		pause.PauseGUI();
+	}
+}
