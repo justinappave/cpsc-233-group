78368dd27dc156d8a17d1497049223f31b555e91
sceveleigh <sceveleigh@gmail.com>
2017-04-12 09:10:28 -0600

    Update Driver.java

--- 0411doc/Driver.java
+++ 0411doc/Driver.java
@@ -2,9 +2,8 @@ import java.awt.*;
 import javax.swing.*;
 
 /*
-names...
-explain what class does...
-parameters, returns
+This class acts as a base to run the program, after each time a new menu is requested
+or the player finishes a game, its comes back here to decide what to do
 */
 
 public class Driver {
@@ -15,34 +14,34 @@ public class Driver {
 
     //Main method (runs game and displays main menu)
 	public static void main(String[] args) {
-        MMenu.setVisible(true);
+        	MMenu.setVisible(true);
 	}
     
-    //Main menu frame
+    //Main menu frame (shows the main menu)
 	public void MainMenu() {
-        MMenu.setVisible(true);
-        LMenu.setVisible(false);
+        	MMenu.setVisible(true);
+        	LMenu.setVisible(false);
 	}
     
-    //Leaderboard menu frame
+    //Leaderboard menu frame (shows the leaderboard menu)
 	public void LeaderboardMenu() {
-        MMenu.setVisible(false);
-        LMenu.setVisible(true);
+        	MMenu.setVisible(false);
+        	LMenu.setVisible(true);
 	}
     
-    //PicturePlay frame
+    //PicturePlay frame (starts picture gametype)
 	public void PicturePlay() {
 		PicturePlay getMode = new PicturePlay(3);
-        MMenu.setVisible(false);
+        	MMenu.setVisible(false);
 	}
     
-    //Freeplay frame
+    //Freeplay frame (starts move count gametype)
 	public void FreePlay() {
 		FreePlay getMode = new FreePlay(3);
-        MMenu.setVisible(false);
+        	MMenu.setVisible(false);
 	}
     
-    //TimePlay frame
+    //TimePlay frame (starts time count gametype)
     public void TimePlay() {
         TimePlay time = new TimePlay(3);
         MMenu.setVisible(false);
